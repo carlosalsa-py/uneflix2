3ee8282c67e7deb797194c436b48ad7a1edce427
carlosalsa-py <carlossalcedo.ea@gmail.com>
2026-05-15 16:38:16 -0400

    watchlist listuki por ahora

--- movies/admin.py
+++ movies/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Genre, Movie
+from .models import Genre, Movie, Watchlist
 
 admin.site.register(Genre)
 admin.site.register(Movie)
+admin.site.register(Watchlist)
