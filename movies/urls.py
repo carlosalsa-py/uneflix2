6d9cd213fb9245b38ac6f684689883e43f17543c
carlosalsa-py <carlossalcedo.ea@gmail.com>
2026-06-04 16:42:16 -0400

    agregado logo, favicon, separe series de peliculas en el home y agregada la opcion de agregar temporadas y capitulos a cada serie

--- movies/urls.py
+++ movies/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('movie/<int:pk>/watchlist/', views.watchlist_toggle, name='watchlist_toggle'),
     path('watchlist/', views.watchlist_view, name='watchlist'),
     path('movie/<int:pk>/play/', views.player_view, name='player'),
+    path('episode/<int:pk>/play/', views.episode_player, name='episode_player'),
     
     # Nuevas rutas para las membresías y la página de pago
     path('membresias/', views.membresias, name='membresias'),
